a3c2518b71d0970f60edca7b200a8e752ce13ee7
YuriFAToledo <yuri.toledo@sou.inteli.edu.br>
2023-08-31 15:25:09 -0300

    feat: task_item

---
+++ tabelaTaskItem.sql
@@ -0,0 +1,11 @@
+CREATE TABLE task_item (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task_id INT,
+    name VARCHAR(255) NOT NULL,
+    `order` INT,
+    completed BOOLEAN,
+    createdAt DATE,
+    created_by INT,
+    completed_at DATE,
+    FOREIGN KEY (task_id) REFERENCES task(id) -- Você precisará especificar a tabela e coluna de referência corretas aqui
+);
